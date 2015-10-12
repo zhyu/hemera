e1ba0b80ef8acab628d56b89eed5fcd71e115688
zhyu <angellwings@gmail.com>
2015-10-12 14:21:42 +0900

    update Nadia, only handle private message now

--- lib/hemera/bot.ex
+++ lib/hemera/bot.ex
@@ -1,7 +1,7 @@
 defmodule Hemera.Bot do
   alias Hemera.Anime
   alias Nadia.Model.Message
-  alias Nadia.Model.User
+  alias Nadia.Model.Chat
 
   @doc """
   pull updates and send to dispatcher
@@ -19,12 +19,19 @@ defmodule Hemera.Bot do
   @doc """
   handle incoming message
   """
-  def handle_message(%Message{chat: %User{id: user_id}, text: "ping"}) do
-    bot_api.send_message(user_id, "pong")
+  def handle_message(%Message{chat: %Chat{type: "private", id: chat_id}, text: text}) do
+    handle_private_message(chat_id, text)
   end
 
-  def handle_message(%Message{chat: %User{id: user_id}, text: "/set_anime_reminder"}) do
-    Anime.add_user(user_id)
+  # fallback
+  def handle_message(_), do: true
+
+  defp handle_private_message(chat_id, "ping") do
+    bot_api.send_message(chat_id, "pong")
+  end
+
+  defp handle_private_message(chat_id, "/set_anime_reminder") do
+    Anime.add_user(chat_id)
 
     reply = """
     If you want to custom tv channels you'd like to watch, you can sign up at:
@@ -34,27 +41,24 @@ defmodule Hemera.Bot do
 
     Otherwise, you will be notified of animes from all tv channels.
     """
-    bot_api.send_message(user_id, reply)
+    bot_api.send_message(chat_id, reply)
   end
 
-  def handle_message(%Message{chat: %User{id: user_id}, text: "/set_anime_user"}) do
-    bot_api.send_message(user_id, "Please specify your username.")
+  defp handle_private_message(chat_id, "/set_anime_user") do
+    bot_api.send_message(chat_id, "Please specify your username.")
   end
 
-  def handle_message(%Message{chat: %User{id: user_id}, text: "/set_anime_user " <> username}) do
-    Anime.add_user(user_id)
-    Anime.set_username(user_id, username)
+  defp handle_private_message(chat_id, "/set_anime_user " <> username) do
+    Anime.add_user(chat_id)
+    Anime.set_username(chat_id, username)
 
     reply = "GJ! You will be notified of animes from tv channels you selected."
-    bot_api.send_message(user_id, reply)
+    bot_api.send_message(chat_id, reply)
   end
 
-  def handle_message(%Message{chat: %User{id: user_id}}) do
-    bot_api.send_sticker(user_id, sticker)
+  defp handle_private_message(chat_id, _) do
+    bot_api.send_sticker(chat_id, sticker)
   end
-
-  # fallback
-  def handle_message(_), do: true
 
   defp bot_api, do: Application.get_env(:hemera, :bot_api)
 
